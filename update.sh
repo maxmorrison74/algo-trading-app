41452b843559cc13adeff5b949dbaf16dea387f2
Max Morrison <maxmorrison@gmail.com>
2026-06-28 02:29:49 +0200

    Silence pip install output in update.sh

--- update.sh
+++ update.sh
@@ -11,7 +11,8 @@ git pull
 
 echo "🚀 Restarting API Server in background..."
 cd backend
-sudo ./venv/bin/python -m pip install -r requirements.txt
+echo "3) Aggiornamento dipendenze Python..."
+sudo ./venv/bin/python -m pip install -q -r requirements.txt
 sudo ./venv/bin/python -m uvicorn api:app --host 0.0.0.0 --port 80 > /dev/null 2>&1 &
 
 echo "✅ Update complete! Server is running."
