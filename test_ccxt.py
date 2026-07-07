480adc112f7a126b9ff1b7d1a63ff532db5ce9f9
Max Morrison <maxmorrison@gmail.com>
2026-07-08 01:51:21 +0200

    fix: rimozione file di stato server da git, aggiornamento .gitignore

--- test_ccxt.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import ccxt.async_support as ccxt
-async def main():
-    b = ccxt.binance()
-    m = await b.load_markets()
-    print("Markets loaded!")
-    await b.close()
-asyncio.run(main())
